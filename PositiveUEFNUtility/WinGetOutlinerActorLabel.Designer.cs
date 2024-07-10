44e2c839444ccc99945f84453ecdfbc0c2a75827
posita <posita333@gmail.com>
2024-07-10 09:38:12 +0900

    Outlinerの選択したアクターの一覧からActorLabelを取得するツールを追加しました

---
+++ PositiveUEFNUtility/WinGetOutlinerActorLabel.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PositiveUEFNUtility
+{
+    partial class WinGetOutlinerActorLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSrcText = new System.Windows.Forms.TextBox();
+            this.textBoxDestText = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSrcText
+            // 
+            this.textBoxSrcText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSrcText.Location = new System.Drawing.Point(13, 35);
+            this.textBoxSrcText.MaxLength = 1000000;
+            this.textBoxSrcText.Multiline = true;
+            this.textBoxSrcText.Name = "textBoxSrcText";
+            this.textBoxSrcText.Size = new System.Drawing.Size(455, 437);
+            this.textBoxSrcText.TabIndex = 0;
+            // 
+            // textBoxDestText
+            // 
+            this.textBoxDestText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDestText.Location = new System.Drawing.Point(555, 35);
+            this.textBoxDestText.MaxLength = 10000000;
+            this.textBoxDestText.Multiline = true;
+            this.textBoxDestText.Name = "textBoxDestText";
+            this.textBoxDestText.Size = new System.Drawing.Size(361, 437);
+            this.textBoxDestText.TabIndex = 1;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConvert.Location = new System.Drawing.Point(474, 218);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 2;
+            this.buttonConvert.Text = "変換";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UEFN情報";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ActorLabel";
+            // 
+            // WinGetOutlinerActorLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 484);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.textBoxDestText);
+            this.Controls.Add(this.textBoxSrcText);
+            this.Name = "WinGetOutlinerActorLabel";
+            this.Text = "WinGetOutlinerActorLabel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSrcText;
+        private System.Windows.Forms.TextBox textBoxDestText;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ PositiveUEFNUtility/WinGetOutlinerActorLabel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PositiveUEFNUtility
+{
+    public partial class WinGetOutlinerActorLabel : Form
+    {
+        public WinGetOutlinerActorLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            if(textBoxSrcText.Text == string.Empty)
+                return;
+
+            string pattern = @"ActorLabel=""([^""]+)""";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(textBoxSrcText.Text);
+
+            List<string> actorLabels = new List<string>();
+            foreach (Match match in matches)
+            {
+                actorLabels.Add(match.Groups[1].Value);
+            }
+
+            // 配列をソート
+            actorLabels.Sort();
+
+            textBoxDestText.Text = string.Join(Environment.NewLine, actorLabels);
+        }
+    }
+}
